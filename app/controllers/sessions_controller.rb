9adea3309d90b13364eccd8bc4ebfcb4debfb576
Shishir Kakaraddi <shishirkakaraddi@gmail.com>
2012-04-14 14:02:18 -0700

    Added facebook login and session creation

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+
+  def create
+    auth_user = User.find_by_fuid (request.env['omniauth.auth']['uid'])
+    if auth_user
+      #User is in our database too already
+    else
+      #He is a new user for our site
+      new_user = User.new
+      new_user.fuid = request.env['omniauth.auth']['uid']
+      new_user.name = request.env['omniauth.auth']['info']['name']
+      new_user.email = request.env['omniauth.auth']['info']['email']
+    end
+  end
+
+end
